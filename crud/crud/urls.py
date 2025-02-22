0e449541ac83a67433a1c826449db75a304193ae
yashchandak101 <chandak.yash101@gmail.com>
2025-02-22 11:30:23 +0530

    Add files via upload

---
+++ crud/crud/urls.py
@@ -0,0 +1,25 @@
+"""
+URL configuration for crud project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from core import views
+urlpatterns = [
+    path('api/products/', views.product_list, name='product_list'),
+    path('api/products/<int:product_id>/', views.get_product_details, name='product_detail'),
+    path('api/submit-feedback/', views.submit_feedback, name='submit_feedback'),
+    path('admin/', admin.site.urls),
+]
